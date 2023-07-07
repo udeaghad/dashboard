07d80d3f01855eb48c6fd6585b2e44495f777d40
udeaghad <dozie.udeagha@gmail.com>
2023-07-07 02:17:59 +0100

    Update product card title and description

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -5,27 +5,32 @@ import { SlBadge } from 'react-icons/sl';
 const ProductCard = ({...product}: IProduct) => {
   
   return (
-    <div className="m-2 border border-stone-600 rounded-md">
-      <div>
-        <div className='z-10 fixed border-2 border-[#272e71] rounded-md flex bg-white justify-center'>
+    <div className="m-3">
+      <div className="relative">
+        <div className='absolute border border-primary rounded-md flex bg-white justify-center top-0 left-0'>
           <div className='bg-[#272e71] p-2'>
             <SlBadge className='text-white text-xl'/>
           </div>
-          <div className='font-bold p-2'>
+          <div className='font-bold p-2 text-primary'>
             {product.type.name}
           </div>
         </div>
-        <img src={product.picture} alt={product.name} className='h-60 w-full z-0'/>        
+        <img src={product.picture} alt={product.name} className='h-60 w-full'/>        
       </div>
 
       <div className="px-2 py-3">
-        <h2 className='font-bold text-[#272e71]'>
+        <h2 className='font-bold text-primary my-3'>
           {
             product.description.split(" ").slice(8).join(" ").charAt(0).toUpperCase() + 
             product.description.split(" ").slice(8,9).join(" ").split('').slice(1).join('') + 
             " " + 
             product.description.split(' ').slice(9, 20).join(" ")
-          }</h2>
+          }
+        </h2>
+
+        <p className="text-primary">
+          {product.description.split(" ").slice(20).join(" ")}
+        </p>
 
       </div>
     
